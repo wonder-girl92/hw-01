ab44e8076c2cd0faef845f4bf15d559211bfd2e0
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2021-09-30 03:43:25 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const sayHi = 'Hello, World!🔆 Меня зовут Юсупова Разет Ибрагимовна❣️'
+ReactDOM.render(sayHi, document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
